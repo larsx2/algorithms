7dca32708d3fd002e7738ffeec7403f4b7a5fd05
Eduardo Urias <windkaiser@gmail.com>
2015-09-22 21:40:12 -0700

    added: palindrome checker using stacks

---
+++ stacks/palindrome.py
@@ -0,0 +1,58 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def empty(self):
+        return self.items == []
+
+    def peek(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            return None
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            return None
+
+    def size(self):
+        return len(self.items)
+
+
+def is_palindrome(s):
+    stack = Stack()
+    for char in s:
+        stack.push(char)
+
+    for i in xrange(len(s)):
+        if s[i] != stack.pop():
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    is_palindrome("aba")
+
+    values = {
+        "palindrome": [
+            "abccba",
+            "aba",
+            "012345543210"
+        ],
+        "non_palindrome": [
+            "abc",
+            "home"
+        ]
+    }
+
+    for value in values["palindrome"]:
+        assert is_palindrome(value)
+
+    for value in values["non_palindrome"]:
+        assert not is_palindrome(value)
