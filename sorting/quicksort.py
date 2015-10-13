a5a018656d0de4c82b3589522b1d2cd99794e4c0
Eduardo Urias <windkaiser@gmail.com>
2015-10-13 10:07:20 -0700

    added: randomized recursive quicksort [Python]

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -12,4 +12,27 @@ def qsort(a):
     return left + [pivot] + right
 
 
-print qsort([9, 8, 7, 3, 1, 2, 5, 4, 6, 8])
+def qsort_randomized(a):
+    A = len(a)
+    if A < 2:
+        return a
+
+    pivot = random.randint(0, A-1)
+    left, right = [], []
+
+    for i in xrange(A):
+        if i == pivot:
+            continue
+
+        if a[i] < a[pivot]:
+            left.append(a[i])
+        else:
+            right.append(a[i])
+
+    return  qsort_randomized(left) + [a[pivot]] + qsort_randomized(right)
+
+
+sample = [9, 8, 7, 3, 1, 2, 5, 4, 6, 8]
+assert qsort(sample) == sorted(sample)
+assert qsort_randomized(sample) == sorted(sample)
+
